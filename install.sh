bf1c711e0f356ed8d1d1a6d68477e602b993c076
Craig L. Ching <craigching@gmail.com>
2011-07-02 12:30:36 -0500

    * Fixing bugs in install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ npm install mongoose
 npm install redis
 npm install connect-redis
 
-mkdir node_modules && cd node_modules
+cd node_modules
 git clone https://github.com/craigching/socket.io.git
 git clone https://github.com/LearnBoost/socket.io-client.git
 cd ..
 npm install uglify-js
 
-DOJO_VERSION=release-1.7.0b2
+DOJO_VERSION=1.7.0b2
 
 curl http://download.dojotoolkit.org/release-$DOJO_VERSION/dojo-release-$DOJO_VERSION-src.tar.gz >dojo-release-$DOJO_VERSION-src.tar.gz
 tar -zxvf dojo-release-$DOJO_VERSION-src.tar.gz
